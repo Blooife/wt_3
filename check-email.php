f8ce205f8b0bd10d99b0f448f3bb51c00f2ef81d
Blooife <beusas.23@gmail.com>
2023-04-04 21:05:13 +0300

    done for now

--- check-email.php
+++ check-email.php
@@ -1,8 +1,6 @@
 <?php
 function check($email) {
-
     if(preg_match("/((^@)+@(^\.)+(\.\S+)+)/", $email)){
-
         return true;
     }else{
         return false;
@@ -13,16 +11,14 @@ function check($email) {
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$message = preg_replace('/(https?:\/\/(?!bsuir\.by)\S+)/', '#Внешние ссылки запрещены#', $message);
+$message = preg_replace('/(https?:\/\/(?!((\S+\.)?(bsuir\.by)))\S+)/', '#Внешние ссылки запрещены#', $message);
 
-if($name != "" && $email != ""){
-
-    if(check($email)){
+if($name != "" && $email != "" && $message != ""){
+   // if(check($email)){
         $file = 'email.txt';
         $data = $name . ', ' . $email . ', ' . $message . PHP_EOL;
         file_put_contents($file, $data, FILE_APPEND);
-    }
-
+  //  }
     header('Location: index.php');
     exit();
 }else{
